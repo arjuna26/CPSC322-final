75c37691a9ef73fc05240c28cbe84fae2f40ec77
arjuna26 <97765080+arjuna26@users.noreply.github.com>
2024-12-11 09:29:49 -0800

    prod app

--- flask/app.py
+++ flask/app.py
@@ -33,4 +33,4 @@ def index():
     return render_template('index.html', winner=None)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
